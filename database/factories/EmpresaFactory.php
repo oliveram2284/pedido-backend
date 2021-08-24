2af70374d4fd7326d5e2f9785f4bfda98ab413df
Mauricio Olivera <feboxp@gmail.com>
2021-08-24 17:39:22 -0300

    Creacion de Empresas

---
+++ database/factories/EmpresaFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Empresa;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmpresaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Empresa::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->unique()->company(),
+            'imagen' => $this->faker->unique()->image(storage_path('images'), $width = 640, $height = 480)
+        ];
+    }
+}
