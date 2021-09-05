22ee8f6d45fd9be064bfcf8b2ccd04ec1bed3351
Mauricio Olivera <feboxp@gmail.com>
2021-09-05 12:10:19 -0300

    Se crea routas apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ComerciosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('comercios', ComerciosController::class);
+//$router->get('comercios',[EmpresaController::class, 'index']);
+/*$router->group(['prefix' => 'api'], function() use ($router)
+{
+
+
+    //$router->post('registro','AuthController@register');
+    //$router->post('login',   'AuthController@login');
+
+});*/
+
+
+
