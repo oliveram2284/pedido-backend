a4726e098d12e20715a40a9c1fcb43c9987c5afc
Mauricio Olivera <feboxp@gmail.com>
2021-09-13 13:13:14 -0300

    Empresa Api controller: se definen metodos index, store y update

--- app/Http/Controllers/Api/ComerciosController.php
+++ app/Http/Controllers/Api/ComerciosController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Empresa;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 class ComerciosController extends Controller
 {
     /**
@@ -14,10 +15,20 @@ class ComerciosController extends Controller
      */
     public function index()
     {
-        $empresas = \App\Models\Empresa::all();
+        $empresas  = \App\Models\Empresa::all();
+
+        foreach ($empresas as $empresa) {
+            $empresa['image_url']    = url('storage/empresas/'.$empresa->imagen);
+            $empresa['public_url']   = asset('empresas/'.$empresa->imagen);
+            $empresa['public_path']  = public_path('empresas/'.$empresa->imagen);
+            $empresa['storage_path'] = storage_path('empresas/'.$empresa->imagen);
+            $empresa['url']          = env('APP_URL', false).Storage::url('empresas/'.$empresa->imagen);
+            //$empresa['temp']         = Storage::get('storage\empresas\\'.$empresa->imagen);
+        }
 
         return response()->json([
-            'comercios'    => $empresas
+            'comercios'    => $empresas,
+            'asset' => url('images/empresas'),
         ]);
     }
 
@@ -39,7 +50,16 @@ class ComerciosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $empresa = new \App\Models\Empresa;
+        $empresa->nombre = $request->input('nombre');
+        $empresa->estado = $request->input('estado');
+        $empresa->save();
+        return response()->json([
+            'status' => 200,
+            'result' => $empresa
+        ]);
+
+
     }
 
     /**
@@ -75,7 +95,15 @@ class ComerciosController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+        $empresa = \App\Models\Empresa::find($id);
+        $empresa->nombre = $request->input('nombre');
+        $empresa->estado = $request->input('estado');
+        $empresa->save();
+        return response()->json([
+            'status' => 200,
+            'result' => true
+        ]);
     }
 
     /**
